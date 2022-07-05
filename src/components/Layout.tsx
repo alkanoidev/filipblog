bf49bf4fd6b67a1d1be55b0707a0001c30f5d790
Filip Ivanovic <alkanoidev@gmail.com>
2022-07-05 17:40:02 +0200

    manifest linked

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -16,6 +16,7 @@ export default function Layout({
         <meta charSet="utf-8" />
         <meta name="Description" content={description}></meta>
         <title>{pageTitle}</title>
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <main>
         {/* <Header /> NAVBAR */}
