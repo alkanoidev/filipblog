bf9e9345b0eb179e5d2e75351294cb67e1a6c4d1
Filip Ivanovic <alkanoidev@gmail.com>
2022-07-12 17:22:25 +0200

    scroll to top btn icon

--- src/components/Buttons/ScrollToTop/ScrollToTop.tsx
+++ src/components/Buttons/ScrollToTop/ScrollToTop.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { AiOutlineArrowUp } from "react-icons/ai";
 
 export default function ScrollToTop() {
   const [classes, setClasses] = useState("");
@@ -20,13 +21,13 @@ export default function ScrollToTop() {
 
   return (
     <button
-      className={`${classes} bg-primary shadow hover:bg-primary/80 rounded-full w-[40px] h-[40px] fixed bottom-[30px] right-[30px] z-50 transition-all`}
+      className={`${classes} bg-primary items-center text-light dark:text-dark justify-center flex text-2xl shadow hover:bg-primary/80 rounded-full w-[40px] h-[40px] fixed bottom-[30px] right-[30px] z-50 transition-all`}
       onClick={() => {
         document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
       }}
     >
-      &uarr;
+      <AiOutlineArrowUp />
     </button>
   );
 }
